aea940bbf995ec456adcdf882f16f2bac2813886
Armin <armin@gwmail.gwu.edu>
2016-01-20 18:21:41 -0500

    Single Neuron added.
    Single neuron added!

--- nest_basics.py
+++ nest_basics.py
@@ -7,3 +7,21 @@ Created on Wed Jan 20 13:53:55 2016
 
 import numpy as np
 import nest
+import pylab
+
+neuron = nest.Create("iaf_neuron")
+multimeter = nest.Create("multimeter")
+nest.SetStatus(neuron, {"I_e": 376.0})
+
+nest.SetStatus(multimeter, {"withtime":True,"record_from":["V_m"]})
+
+spikedetector = nest.Create("spike_detector", params={"withgid":True, "withtime": True})
+
+nest.Connect(multimeter, neuron)
+nest.Connect(neuron, spikedetector)
+
+Armin = nest.Simulate(1000.0)
+
+dmm = nest.GetStatus(multimeter)[0]
+Vms = dmm["events"]["V_m"]
+ts = dmm["events"]["times"]
